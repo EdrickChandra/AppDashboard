73fb5886b18e9c380426bd2792893b1a12303e62
EdrickChandra <edrick2807@gmail.com>
2025-03-18 13:22:15 +0700

    Cleaning page

---
+++ Surveying/Models/PhotoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surveying.Models
+{
+    public class PhotoItem
+    {
+        public FileResult FileResult { get; set; }
+        public ImageSource ImageSource { get; set; }
+    }
+
+}
--- Surveying/Views/Cleaning.xaml.cs
+++ Surveying/Views/Cleaning.xaml.cs
@@ -1,9 +1,54 @@
+using Surveying.Models;
+using System.Collections.ObjectModel;
+
 namespace Surveying.Views;
 
 public partial class Cleaning : ContentPage
 {
-	public Cleaning()
+
+    public ObservableCollection<PhotoItem> Photos { get; set; } = new ObservableCollection<PhotoItem>();
+
+    public Cleaning()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+
+        BindingContext = this;
+    }
+
+    private async void OnUploadPhotoClicked(object sender, EventArgs e)
+    {
+        try
+        {
+          
+            var photoResult = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Select a Photo"
+            });
+
+            if (photoResult == null)
+                return;
+
+            using var stream = await photoResult.OpenReadAsync();
+            var imageSource = ImageSource.FromStream(() => stream);
+
+            Photos.Add(new PhotoItem
+            {
+                FileResult = photoResult,
+                ImageSource = imageSource
+            });
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.BindingContext is PhotoItem photo)
+        {
+
+            Photos.Remove(photo);
+        }
+    }
 }
