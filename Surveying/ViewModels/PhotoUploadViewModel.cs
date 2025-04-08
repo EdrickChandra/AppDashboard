e47a3db5dc02f6d580db97eedca43e731c6f23aa
EdrickChandra <edrick2807@gmail.com>
2025-04-08 15:29:42 +0700

    Update photo and UI

--- Surveying/ViewModels/PhotoUploadViewModel.cs
+++ Surveying/ViewModels/PhotoUploadViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui;
 
 namespace Surveying.ViewModels
 {
@@ -12,23 +13,72 @@ namespace Surveying.ViewModels
     {
         public ObservableCollection<PhotoItem> Photos { get; set; } = new ObservableCollection<PhotoItem>();
 
-       
+        // Property for column count that will be used in XAML
+        [ObservableProperty]
+        private int _photoColumnCount;
+
+        // The threshold width in device-independent pixels
+        private const double MobileWidthThreshold = 768;
+
         public IAsyncRelayCommand UploadPhotoAsyncCommand => new AsyncRelayCommand(UploadPhotoAsync);
         public IRelayCommand DeletePhotoCommand => new RelayCommand<PhotoItem>(DeletePhoto);
+
+        public PhotoUploadViewModel()
+        {
+            // Set initial column count based on current screen size
+            UpdateColumnCount();
+
+            // Listen for orientation or size changes
+            DeviceDisplay.MainDisplayInfoChanged += OnDisplayInfoChanged;
+        }
+
+        private void OnDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
+        {
+            UpdateColumnCount();
+        }
+
+        private void UpdateColumnCount()
+        {
+           
+            double screenWidth = DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density;
+
+        
+            PhotoColumnCount = screenWidth <= MobileWidthThreshold ? 1 : 3;
+
+            System.Diagnostics.Debug.WriteLine($"Screen width: {screenWidth}, Column count: {PhotoColumnCount}");
+        }
 
         public async Task UploadPhotoAsync()
         {
             try
             {
-         
                 await Microsoft.Maui.ApplicationModel.MainThread.InvokeOnMainThreadAsync(async () =>
                 {
-                    System.Diagnostics.Debug.WriteLine("Calling MediaPicker.PickPhotoAsync...");
-                    var photoResult = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                    
+                    double screenWidth = DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density;
+
+                    FileResult photoResult;
+
+                    // If screen width is less than or equal to 768, use camera
+                    if (screenWidth <= MobileWidthThreshold)
                     {
-                        Title = "Select a Photo"
-                    });
-                    System.Diagnostics.Debug.WriteLine("MediaPicker returned: " + (photoResult != null ? "Photo Selected" : "Null"));
+                        System.Diagnostics.Debug.WriteLine("Using camera for mobile device...");
+                        photoResult = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions
+                        {
+                            Title = "Take a Photo"
+                        });
+                        System.Diagnostics.Debug.WriteLine("Camera result: " + (photoResult != null ? "Photo Captured" : "Null"));
+                    }
+                    // Otherwise use gallery picker
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("Using gallery picker for larger screen...");
+                        photoResult = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                        {
+                            Title = "Select a Photo"
+                        });
+                        System.Diagnostics.Debug.WriteLine("Gallery result: " + (photoResult != null ? "Photo Selected" : "Null"));
+                    }
 
                     if (photoResult == null)
                         return;
